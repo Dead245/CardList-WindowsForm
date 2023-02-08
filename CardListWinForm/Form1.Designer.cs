b911ecf251cb21de2ed61240663e66afc3c5a1fd
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-02-08 02:41:14 -0600

    Add project files.

---
+++ CardListWinForm/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CardListWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.cardListBox = new System.Windows.Forms.ListBox();
+            this.cardInfoLabel = new System.Windows.Forms.Label();
+            this.cardListLabel = new System.Windows.Forms.Label();
+            this.cardInfoTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(188, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchTextBox.Location = new System.Drawing.Point(12, 12);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(170, 23);
+            this.searchTextBox.TabIndex = 1;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // cardListBox
+            // 
+            this.cardListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardListBox.FormattingEnabled = true;
+            this.cardListBox.ItemHeight = 16;
+            this.cardListBox.Location = new System.Drawing.Point(13, 58);
+            this.cardListBox.Name = "cardListBox";
+            this.cardListBox.Size = new System.Drawing.Size(250, 372);
+            this.cardListBox.TabIndex = 2;
+            this.cardListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // cardInfoLabel
+            // 
+            this.cardInfoLabel.AutoSize = true;
+            this.cardInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardInfoLabel.Location = new System.Drawing.Point(595, 38);
+            this.cardInfoLabel.Name = "cardInfoLabel";
+            this.cardInfoLabel.Size = new System.Drawing.Size(112, 17);
+            this.cardInfoLabel.TabIndex = 3;
+            this.cardInfoLabel.Text = "Card Information";
+            this.cardInfoLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cardListLabel
+            // 
+            this.cardListLabel.AutoSize = true;
+            this.cardListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardListLabel.Location = new System.Drawing.Point(95, 38);
+            this.cardListLabel.Name = "cardListLabel";
+            this.cardListLabel.Size = new System.Drawing.Size(64, 17);
+            this.cardListLabel.TabIndex = 5;
+            this.cardListLabel.Text = "Card List";
+            // 
+            // cardInfoTextBox
+            // 
+            this.cardInfoTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.cardInfoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardInfoTextBox.Location = new System.Drawing.Point(491, 58);
+            this.cardInfoTextBox.Name = "cardInfoTextBox";
+            this.cardInfoTextBox.Size = new System.Drawing.Size(297, 377);
+            this.cardInfoTextBox.TabIndex = 6;
+            this.cardInfoTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cardInfoTextBox);
+            this.Controls.Add(this.cardListLabel);
+            this.Controls.Add(this.cardInfoLabel);
+            this.Controls.Add(this.cardListBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ListBox cardListBox;
+        private System.Windows.Forms.Label cardInfoLabel;
+        private System.Windows.Forms.Label cardListLabel;
+        private System.Windows.Forms.RichTextBox cardInfoTextBox;
+    }
+}
+
---
+++ CardListWinForm/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardListWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<object> originalList = new List<object>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<int> searchResultIndexs = new List<int>();
+
+            string textSearch = searchTextBox.Text;
+
+            //Search for results in the list
+            for (int i = 0; i < originalList.Count; i++)
+            {
+                string item = originalList[i].ToString();
+                if (item.ToLower().Contains(textSearch.ToLower())) {
+                    searchResultIndexs.Add(i);
+                }
+            }
+
+            //Show results of search
+            int searchLength = searchResultIndexs.Count;
+            List<object> searchedItems = new List<object>();
+            for (int i = 0; i < searchLength; i++)
+            {
+                searchedItems.Add(originalList[searchResultIndexs[i]]);
+            }
+            
+            cardListBox.Items.Clear();
+
+            for (int i = 0; i < searchLength; i++)
+            {
+                cardListBox.Items.Add(searchedItems[i]);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ALL TEMPORARY
+            cardListBox.Items.Add("The One");
+            cardListBox.Items.Add("The Second");
+            cardListBox.Items.Add("That Third One");
+            cardListBox.Items.Add("The Fourth");
+            cardListBox.Items.Add("Those Fifths");
+            cardListBox.Items.Add("That Sixth");
+            cardListBox.Items.Add("Those Sevenths");
+            cardListBox.Items.Add("The Eighth");
+            cardListBox.Items.Add("Nine");
+            cardListBox.Items.Add("Ten");
+
+            for (int i = 0; i < cardListBox.Items.Count; i++)
+            {
+                originalList.Add(cardListBox.Items[i]);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Show info of selected card on cardInfoTextBox
+
+            //variables for Card Information Box
+            string cardName = "No Name", cardCost = "No Cost", cardType = "No Type",
+                   cardAttributes = "No Attributes", cardDescription = "No Description", cardFlavorText = "No Flavor Text";
+
+            cardInfoTextBox.Clear();
+            cardInfoTextBox.Text = "Name:\n\t " + cardName + "\n" +
+                                   "Cost:\n\t " + cardCost + "\n" +
+                                   "Type:\n\t " + cardType + "\n" +
+                                   "Attributes:\n\t " + cardAttributes + "\n" +
+                                   "Description:\n\t " + cardDescription + "\n" +
+                                   "Flavor Text:\n\t " + cardFlavorText;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void cardInfoTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
